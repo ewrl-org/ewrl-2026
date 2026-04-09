8dc07f1767c0076e419738fecc97c17af823a1f6
TimotheeMathieu <timothee.mathieu@inria.fr>
2026-04-09 09:46:52 +0200

    tba typo

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def call():
     data["call"] = open("sitedata/call.md").read()
     return render_template("call.html", **data)
 
-@app.route("/tba_schedule.html.html")
+@app.route("/tba_schedule.html")
 def call():
     data = _data()
     return render_template("tba_schedule.html", **data)
